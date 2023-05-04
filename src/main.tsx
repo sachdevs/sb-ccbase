947c73ef4272d43463c6f1f4e17e53feed6089d9
sachdevs <sachdev.96@gmail.com>
2023-05-04 16:22:49 -0400

    cc base

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function Component({
+  text,
+  onClick,
+  updateText,
+}: {
+  text: string;
+  onClick: () => Promise<void>;
+  updateText: (t: string) => void;
+}) {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div style={{ background: "#fafafa" }}>
+      <h1>Welcome Custom Component</h1>
+      <h1>{text}</h1>
+      <button onClick={() => setCount((x) => x + 2)}>
+        React State Clicked: {count}
+      </button>
+      <button
+        onClick={() => {
+          updateText(text + "!");
+        }}
+      >
+        Add text to header
+      </button>
+      <button onClick={onClick}>Fire testing onClick</button>
+      <App />
+      <br />
+      <div>This content is being served on a different server.</div>
+      <div style={{ background: "white" }}>
+        {" "}
+        Look, it even hot reloads! Just develop!
+      </div>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteExternalsPlugin } from "vite-plugin-externals";
+import { appendHotReloadEventPlugin } from "@superblocksteam/vite-custom-component-reload-plugin";
 
+const port = 3002;
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+      preserveEntrySignatures: "strict",
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+    copyPublicDir: true,
+    write: true,
+  },
+  publicDir: "../host",
+
+  base: `http://localhost:${port}/`,
+  server: {
+    port: port,
+  },
+  plugins: [
+    react(),
+    viteExternalsPlugin({
+      react: "React",
+      "react-dom": "ReactDOM",
+    }),
+    appendHotReloadEventPlugin(),
+  ],
+});
